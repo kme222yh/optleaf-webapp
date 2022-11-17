7bb6fc7a88e1f4a3a8b11551285d9afab0914cfc
kme222yh <49933526+kme222yh@users.noreply.github.com>
2022-11-17 19:57:11 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { reactScopedCssPlugin } from 'rollup-plugin-react-scoped-css';
 
@@ -17,5 +17,25 @@ export default defineConfig({
             }
         }
     },
-    plugins: [react(), reactScopedCssPlugin()]
+    plugins: [react(), reactScopedCssPlugin(), htmlPlugin(loadEnv('dev', "."))]
 });
+
+
+
+
+/**
+ * Replace env variables in index.html
+ * @see https://github.com/vitejs/vite/issues/3105#issuecomment-939703781
+ * @example `%VITE_MY_ENV%`
+ * @see https://vitejs.dev/guide/api-plugin.html#transformindexhtml
+ */
+ function htmlPlugin(env: ReturnType<typeof loadEnv>) {
+    return {
+      name: "html-transform",
+      transformIndexHtml: {
+        enforce: "pre" as const,
+        transform: (html: string): string =>
+          html.replace(/%(.*?)%/g, (match, p1) => env[p1] ?? match),
+      },
+    };
+  }
