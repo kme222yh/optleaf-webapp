81fa81203ae6532261dd26a11856bb5e272ca794
kme222yh <49933526+kme222yh@users.noreply.github.com>
2022-11-27 23:32:17 +0900

    loading view

--- src/views/LoadingView/LoadingView.tsx
+++ src/views/LoadingView/LoadingView.tsx
@@ -1,9 +1,13 @@
 import './LoadingView.scss';
 
+import OptleafLogo from '@/assets/images/logo/white.svg';
+
 export function LoadingView() {
     return (
         <div className="LoadingView">
-            <div className="LoadingView-body" />
+            <div className="LoadingView-body">
+                <img src={OptleafLogo} alt="Optleaf logo" />
+            </div>
         </div>
     );
 }
